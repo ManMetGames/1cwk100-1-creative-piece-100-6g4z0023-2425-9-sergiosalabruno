34078ae41d0130d33fc54b39d635d92e43ff4f20
sergiosalabruno <21402535@stu.mmu.ac.uk>
2024-10-01 18:03:13 +0100

    initial commit
    end of class 1

---
+++ Assets/ballcontroller.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballcontroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float forceMult = 10.0f;
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 force = new Vector3(h, 0.0f, v) * forceMult;
+        GetComponent<Rigidbody>().AddForce(force);
+    }
+}
